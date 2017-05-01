c15da7e8e0f2f0b4c221790a9cae7d3ee7d37bcd
tomhup <tomhup@users.noreply.github.com>
2017-05-01 13:44:04 +0800

    Update HeartbeatFreq.py

--- HeartbeatFreq.py
+++ HeartbeatFreq.py
@@ -54,9 +54,7 @@ if __name__ == '__main__':
         if len(rects) > 0:
             x1, y1, x2, y2 = rects[0]
             xx1, yy1, xx2, yy2 = int((x1+x2)/2 - (x2-x1)*0.1), int(y1+(y2-y1)*0.1),  int((x1+x2)/2+(x2-x1)*0.1), int(y1+(y2-y1)*0.2)
-
             gg = img[x1:x2, y1:y2, 1]
-
             if q.full():
                 q.get()
                 q.put(gg)
@@ -83,15 +81,13 @@ if __name__ == '__main__':
             frez[0:20] = 0
             tf = frez[0:len(frez)/2]
             tf = np.where(tf == max(tf))
-            tf = tf[0]*ft/q.qsize()*10
+            tf = tf[0]*ft/Sample_Num*10
             if q2.full():
                 q2.get()
                 q2.put(tf)
             else:
                 q2.put(tf)
-
             tf = np.average(np.array(q2.queue))
-
         draw_str(vis, (20, 20), 'Sample Freq: %.0f Heartbeat Freq: %.0f Sample Num: %d '%(ft,tf, len(zz)))
         cv2.imshow('Heartbeat frequency', vis)
         if 0xFF & cv2.waitKey(5) == 27:
